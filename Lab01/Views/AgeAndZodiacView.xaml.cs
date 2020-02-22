80c887d1b39502a96770c835963df52534fa6e90
Dan Darituan <darituan8@gmail.com>
2020-02-22 22:22:27 +0200

    add AgeAndZodiacView xaml and code behind

---
+++ Lab01/Views/AgeAndZodiacView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Lab01.ViewModels.AgeAndZodiac;
+
+namespace Lab01.Views
+{
+    public partial class AgeAndZodiacView : UserControl
+    {
+        public AgeAndZodiacView()
+        {
+            InitializeComponent();
+            DataContext = new AgeAndZodiacViewModel();
+        }
+    }
+}
