42f6a69485127e161559e6c9b031898d206e2f9e
Dan Darituan <darituan8@gmail.com>
2020-02-22 22:20:31 +0200

    add MainWindowViewModel

---
+++ Lab01/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using Lab01.Tools;
+using Lab01.Tools.Managers;
+
+namespace Lab01.ViewModels
+{
+    internal class MainWindowViewModel: BaseViewModel, ILoaderOwner
+    {
+        #region Fields
+        private Visibility _loaderVisibility = Visibility.Hidden;
+        private bool _isControlEnabled = true;
+        #endregion
+
+        #region Properties
+        public Visibility LoaderVisibility
+        {
+            get { return _loaderVisibility; }
+            set
+            {
+                _loaderVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsControlEnabled
+        {
+            get { return _isControlEnabled; }
+            set
+            {
+                _isControlEnabled = value;
+                OnPropertyChanged();
+            }
+        } 
+        #endregion
+
+        internal MainWindowViewModel()
+        {
+            LoaderManager.Instance.Initialize(this);
+        } 
+    }
+}
